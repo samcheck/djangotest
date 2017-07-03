6a1b6641cdf326987bba683a67f0b22aeb5b56cf
samcheck <samcheckster@gmail.com>
2017-07-03 15:14:13 -0500

    Adding poll views/urls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/4/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/4/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/4/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Allo, this is the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at the results of question {}."
+    return HttpResponse(response.format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}.".format(question_id))
